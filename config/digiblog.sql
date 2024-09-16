8a6a6a628ed900cea5def84e5ada6c23368325c3
digiflore <ddigiflore@gmail.com>
2024-09-16 14:53:13 +0200

    New database

--- config/digiblog.sql
+++ config/digiblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 11 sep. 2024 à 04:46
+-- Généré le : lun. 16 sep. 2024 à 12:52
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -24,11 +24,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `comments`
+-- Structure de la table `dg_comment`
 --
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
+DROP TABLE IF EXISTS `dg_comment`;
+CREATE TABLE IF NOT EXISTS `dg_comment` (
   `id` int NOT NULL AUTO_INCREMENT,
   `post_id` int NOT NULL,
   `author` varchar(255) NOT NULL,
@@ -36,26 +36,50 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment` text NOT NULL,
   `comment_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `comments`
+-- Déchargement des données de la table `dg_comment`
 --
 
-INSERT INTO `comments` (`id`, `post_id`, `author`, `mail`, `comment`, `comment_date`) VALUES
+INSERT INTO `dg_comment` (`id`, `post_id`, `author`, `mail`, `comment`, `comment_date`) VALUES
 (1, 1, 'Mathieu', '', 'Premier commentaire ! Bienvenue et bon courage pour la suite.', '2022-03-03 13:00:42'),
 (2, 1, 'Sam', '', 'Hâte de voir comment ce blog évolue !', '2024-09-08 13:01:42'),
 (3, 1, 'moi', '', 'C\'est super !', '2024-09-03 16:46:28'),
-(4, 2, 'Moi', '', 'Bon courage !\r\n', '2024-09-11 00:19:07');
+(4, 2, 'Moi', '', 'Bon courage à toi !\r\n', '2024-09-11 00:19:07'),
+(12, 1, 'cc', 'cc', 'ccc', '2024-09-15 10:45:07'),
+(13, 1, 'cc', 'cc', 'ccc', '2024-09-15 10:45:59'),
+(14, 1, 'cc', 'cc', 'ccc', '2024-09-15 10:48:57'),
+(15, 1, 'cc', 'cc', 'ccc', '2024-09-15 10:49:05'),
+(16, 2, 'd', 'd', 'Super\r\n', '2024-09-15 10:51:03'),
+(17, 2, 'd', 'd', 'Super\r\n', '2024-09-15 10:51:54'),
+(18, 2, 'd', 'd', 'Super\r\n', '2024-09-15 10:52:52'),
+(19, 2, 'd', 'd', 'Super\r\n', '2024-09-15 10:53:15'),
+(20, 2, 'd', 'd', 'Super\r\n', '2024-09-15 10:53:44'),
+(21, 2, 'd', 'd', 'Super\r\n', '2024-09-15 10:53:52'),
+(22, 2, 'd', 'd', 'cccc', '2024-09-15 10:54:05'),
+(23, 2, 'd', 'd', 'xxcxcx', '2024-09-15 10:55:39'),
+(24, 2, 'd', 'd', 'xxcxcx', '2024-09-15 10:56:25'),
+(25, 2, 'c', 'c', 'cswwscw', '2024-09-15 10:56:33'),
+(26, 2, 'ccc', 'ccc', 'ccc', '2024-09-15 11:52:36'),
+(27, 2, 'c', 'c', 'cccc', '2024-09-16 09:35:26'),
+(28, 2, 'c', 'c', 'cccc', '2024-09-16 14:16:55'),
+(29, 2, 'c', 'c', 'cccc', '2024-09-16 14:16:59'),
+(30, 2, 'dc', 'd', 'd', '2024-09-16 14:17:08'),
+(31, 2, 'dc', 'd', 'd', '2024-09-16 14:18:21'),
+(32, 2, 'dc', 'd', 'd', '2024-09-16 14:19:44'),
+(33, 2, 'sss', 'ssss', 'ssss', '2024-09-16 14:20:45'),
+(34, 2, 'dddd', 'ddd', 'ddd', '2024-09-16 14:21:38'),
+(35, 2, 'c', 'c', 'cccc', '2024-09-16 14:46:45');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `posts`
+-- Structure de la table `dg_post`
 --
 
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE IF NOT EXISTS `posts` (
+DROP TABLE IF EXISTS `dg_post`;
+CREATE TABLE IF NOT EXISTS `dg_post` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
@@ -64,12 +88,60 @@ CREATE TABLE IF NOT EXISTS `posts` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 --
--- Déchargement des données de la table `posts`
+-- Déchargement des données de la table `dg_post`
 --
 
-INSERT INTO `posts` (`id`, `title`, `content`, `creation_date`) VALUES
+INSERT INTO `dg_post` (`id`, `title`, `content`, `creation_date`) VALUES
 (1, 'Bienvenue sur Digiblog !', 'Je vous souhaite à toutes et à tous la bienvenue sur le blog de DigiFlore, Digiblog !\r\nCe blog est conçu en MVC PHP et est en évolution permanente.\r\nIl répondra je l\'espère à vos besoins les plus fou.', '2024-09-08 16:28:41'),
 (2, 'Comment à débuter Digiblog ?', 'Digiblog à débuter d\'une volonté de m\'améliorer dans les outils informatiques. Ingénieur en développement informatique, je cherche à me perfectionner dans les outils actuelles. Et créer un blog est un gros défi pour moi.', '2024-09-11 16:28:42');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dg_right`
+--
+
+DROP TABLE IF EXISTS `dg_right`;
+CREATE TABLE IF NOT EXISTS `dg_right` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `dg_right`
+--
+
+INSERT INTO `dg_right` (`id`, `name`) VALUES
+(1, 'DG_ADMIN'),
+(2, 'DG_WRITER'),
+(3, 'DG_USER');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dg_user`
+--
+
+DROP TABLE IF EXISTS `dg_user`;
+CREATE TABLE IF NOT EXISTS `dg_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `lastname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `mail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `rights_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rights_id` (`rights_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `dg_user`
+--
+
+INSERT INTO `dg_user` (`id`, `firstname`, `lastname`, `password`, `mail`, `username`, `rights_id`) VALUES
+(1, 'flore', '', '0', 'flore@gmail.com', '0', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
